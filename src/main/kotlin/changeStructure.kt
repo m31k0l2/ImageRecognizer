bb64a47aca86f8eb30aa99ac0e20b10edd8f1270
ih8unem <ih8unem@gmail.com>
2018-04-10 11:04:34 +0300

    fix

--- src/main/kotlin/changeStructure.kt
+++ src/main/kotlin/changeStructure.kt
@@ -1,17 +1,16 @@
 val image = MNIST.buildBatch(10).first()
 
 fun main(args: Array<String>) {
-    changeStructure("nets/nw4.net", "nets/nw.net")
+    changeStructure("nets/nw.net", "nets/nw.net")
 }
 
 fun changeStructure(from: String, to: String) {
     val nw = NetworkIO().load(from)!!
-    val net = CNetwork(0, 0, 0, 0, 0, 10)
+    val net = CNetwork(0, 0, 0, 0, 20, 10)
     net.layers[0].neurons.addAll(nw.layers[0].neurons)
     net.layers[1].neurons.addAll(nw.layers[1].neurons)
     net.layers[2].neurons.addAll(nw.layers[2].neurons)
     net.layers[3].neurons.addAll(nw.layers[3].neurons)
-    net.layers[4].neurons.addAll(nw.layers[4].neurons)
     net.activate(image)
     NetworkIO().save(net, to)
 }
--- src/main/kotlin/train.kt
+++ src/main/kotlin/train.kt
@@ -1,36 +1,62 @@
+import java.util.*
+import java.util.logging.FileHandler
+import java.util.logging.Logger
+import java.util.logging.SimpleFormatter
+
+val log = Logger.getLogger("logger")
+val fh = FileHandler("log.txt")
+
 fun main(args: Array<String>) {
-    val testNumbers = (0..4).toList()
-    train(testNumbers, emptyList(), 7)
+    log.addHandler(fh)
+    fh.formatter = SimpleFormatter()
+    val testBatch = MNIST.buildBatch(1000)
+    val testNumbers = (0..5).toList()
+    for (rateCount in 3..5 step 2) {
+        train(testNumbers, listOf(0), rateCount, testBatch)
+        train(testNumbers, listOf(1), rateCount, testBatch)
+        train(testNumbers, listOf(2), rateCount, testBatch)
+        train(testNumbers, listOf(3), rateCount, testBatch)
+        train(testNumbers, listOf(4), rateCount, testBatch)
+        train(testNumbers, listOf(5), rateCount, testBatch)
+        train(testNumbers, listOf(4, 5), rateCount, testBatch)
+        train(testNumbers, listOf(0, 4, 5), rateCount, testBatch)
+        train(testNumbers, listOf(1, 4, 5), rateCount, testBatch)
+        train(testNumbers, listOf(2, 4, 5), rateCount, testBatch)
+        train(testNumbers, listOf(3, 4, 5), rateCount, testBatch)
+        train(testNumbers, (0..3).toList(), rateCount, testBatch)
+        train(testNumbers, emptyList(), rateCount, testBatch)
+    }
 }
 
-private fun train(testNumbers: List<Int>, trainLayers: List<Int>, rateCount: Int) {
+private fun train(testNumbers: List<Int>, trainLayers: List<Int>, rateCount: Int, testBatch: List<Image>) {
+    println("trainLayers: $trainLayers")
+    log.info("trainLayers: $trainLayers, rateCount: $rateCount")
     val net = ImageNetEvolution(rateCount)
     net.trainLayers = trainLayers
-    val addBatchSize = 500
+    val addBatchSize = 200
     net.name = "nets/nw.net"
     net.mutantStrategy = { e, _ ->
         when {
-            e < 100 -> ((100-e)/100.0)
-            else -> 0.2
+            e < 50 -> ((50-e)/50.0)
+            else -> 0.2*Random().nextDouble()
         }
     }
     net.batch = MNIST.buildBatch(addBatchSize).filter { it.index in testNumbers }
-    var res2 = NetworkIO().load(net.name)?.let { testMedianNet(it, net.batch) } ?: 0.0
-    var errorCounter = 0
+    var res2 = NetworkIO().load(net.name)?.let { testMedianNet(it, testBatch) } ?: 0.0
     for (i in 1..10) {
         println("$i) ${net.batch.size}")
-        net.evolute(1000, 80, 10)
-        val res = testMedianNet(net.leader!!.nw, net.batch)
+        net.evolute(150, 160, 10)
+        val res = testMedianNet(net.leader!!.nw, testBatch)
         if (res > res2) {
             NetworkIO().save(net.leader!!.nw, net.name)
             println("SAVE")
+            log.info("SAVE")
             res2 = res
-            errorCounter = 0
         } else {
             println("NO SAVE")
-            if (errorCounter == 2) return
-            errorCounter++
+            log.info("NO SAVE")
             NetworkIO().save(net.leader!!.nw, "nets/_nw.net")
+            break
         }
         net.batch = net.batch.union(MNIST.buildBatch(addBatchSize).filter { it.index in testNumbers }).toList()
     }
