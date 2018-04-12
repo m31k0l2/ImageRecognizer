a19c032e59a48ef292a6140f8fdfd2753783707c
ih8unem <ih8unem@gmail.com>
2018-04-12 08:49:38 +0300

    fix

--- src/main/kotlin/changeStructure.kt
+++ src/main/kotlin/changeStructure.kt
@@ -6,11 +6,13 @@ fun main(args: Array<String>) {
 
 fun changeStructure(from: String, to: String) {
     val nw = NetworkIO().load(from)!!
-    val net = CNetwork(0, 0, 0, 0, 20, 10)
+    val net = CNetwork(0, 0, 4, 0, 0, 0)
     net.layers[0].neurons.addAll(nw.layers[0].neurons)
     net.layers[1].neurons.addAll(nw.layers[1].neurons)
-    net.layers[2].neurons.addAll(nw.layers[2].neurons)
+//    net.layers[2].neurons.addAll(nw.layers[2].neurons)
     net.layers[3].neurons.addAll(nw.layers[3].neurons)
+    net.layers[4].neurons.addAll(nw.layers[4].neurons)
+    net.layers[5].neurons.addAll(nw.layers[5].neurons)
     net.activate(image)
     NetworkIO().save(net, to)
 }
--- src/main/kotlin/train.kt
+++ src/main/kotlin/train.kt
@@ -1,4 +1,3 @@
-import java.util.*
 import java.util.logging.FileHandler
 import java.util.logging.Logger
 import java.util.logging.SimpleFormatter
@@ -10,54 +9,35 @@ fun main(args: Array<String>) {
     log.addHandler(fh)
     fh.formatter = SimpleFormatter()
     val testBatch = MNIST.buildBatch(1000)
-    val testNumbers = (0..5).toList()
-    for (rateCount in 3..5 step 2) {
-        train(testNumbers, listOf(0), rateCount, testBatch)
-        train(testNumbers, listOf(1), rateCount, testBatch)
-        train(testNumbers, listOf(2), rateCount, testBatch)
-        train(testNumbers, listOf(3), rateCount, testBatch)
-        train(testNumbers, listOf(4), rateCount, testBatch)
-        train(testNumbers, listOf(5), rateCount, testBatch)
-        train(testNumbers, listOf(4, 5), rateCount, testBatch)
-        train(testNumbers, listOf(0, 4, 5), rateCount, testBatch)
-        train(testNumbers, listOf(1, 4, 5), rateCount, testBatch)
-        train(testNumbers, listOf(2, 4, 5), rateCount, testBatch)
-        train(testNumbers, listOf(3, 4, 5), rateCount, testBatch)
-        train(testNumbers, (0..3).toList(), rateCount, testBatch)
-        train(testNumbers, emptyList(), rateCount, testBatch)
-    }
+    val testNumbers = (0..4).toList()
+    train(testNumbers, listOf(), 3, testBatch, 500, false, false)
 }
 
-private fun train(testNumbers: List<Int>, trainLayers: List<Int>, rateCount: Int, testBatch: List<Image>) {
-    println("trainLayers: $trainLayers")
+private fun train(testNumbers: List<Int>, trainLayers: List<Int>, rateCount: Int, testBatch: List<Image>, addBatchSize: Int, isAdder: Boolean, isUpdated: Boolean = false) {
     log.info("trainLayers: $trainLayers, rateCount: $rateCount")
     val net = ImageNetEvolution(rateCount)
     net.trainLayers = trainLayers
-    val addBatchSize = 200
     net.name = "nets/nw.net"
     net.mutantStrategy = { e, _ ->
         when {
             e < 50 -> ((50-e)/50.0)
-            else -> 0.2*Random().nextDouble()
+            else -> 0.2
         }
     }
     net.batch = MNIST.buildBatch(addBatchSize).filter { it.index in testNumbers }
     var res2 = NetworkIO().load(net.name)?.let { testMedianNet(it, testBatch) } ?: 0.0
     for (i in 1..10) {
-        println("$i) ${net.batch.size}")
-        net.evolute(150, 160, 10)
+        net.evolute(150, 60, 10)
         val res = testMedianNet(net.leader!!.nw, testBatch)
         if (res > res2) {
             NetworkIO().save(net.leader!!.nw, net.name)
-            println("SAVE")
-            log.info("SAVE")
+            log.info("SAVE, batch: ${net.batch.size}, res = $res [old $res2]")
             res2 = res
         } else {
-            println("NO SAVE")
-            log.info("NO SAVE")
+            log.warning("NO SAVE, batch: ${net.batch.size}, res = $res [old $res2]")
             NetworkIO().save(net.leader!!.nw, "nets/_nw.net")
-            break
         }
-        net.batch = net.batch.union(MNIST.buildBatch(addBatchSize).filter { it.index in testNumbers }).toList()
+        if (isAdder) net.batch = net.batch.union(MNIST.buildBatch(addBatchSize).filter { it.index in testNumbers }).toList()
+        else if (isUpdated) net.batch = MNIST.buildBatch(addBatchSize).filter { it.index in testNumbers }
     }
 }
